5c928665eb6d04ba578a6757fa17fece6a2f32e6
HarounGazzah <haroun.gazzah@gmail.com>
2019-01-17 21:55:21 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DHMIT-Test';
+
+
+persons: any[] = [
+  { id: '001', name: 'Mr. Moez' },
+  { id: '002', name: 'Mr. Tarek' },
+  { id: '003', name: 'Haroun' },
+  { id: '004', name: 'Walid' },
+  { id: '005', name: 'Ahmed' },
+  { id: '006', name: 'Ilyess' },
+  { id: '007', name: 'Omar' },
+];
+
 }
